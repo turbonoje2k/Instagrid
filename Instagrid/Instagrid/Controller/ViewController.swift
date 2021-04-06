f060bbc0ecbfff462b0baad8908339c8c32db949
Noje <9hnoje@gmail.com>
2021-04-06 18:08:47 +0200

    finish the vertical View

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonLayout1: UIButton!
+    @IBOutlet weak var buttonLayout2: UIButton!
+    @IBOutlet weak var buttonLayout3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonLayout1.setImage(UIImage(named: "Layout1"), for: .normal)
+        buttonLayout2.setImage(UIImage(named: "Layout2"), for: .normal)
+        buttonLayout3.setImage(UIImage(named: "Layout3"), for: .normal)
+        
+        buttonLayout1.setImage(UIImage(named: "layoutSelected"), for: .selected)
+        buttonLayout2.setImage(UIImage(named: "layoutSelected2"), for: .selected)
+        buttonLayout3.setImage(UIImage(named: "layoutSelected3"), for: .selected)
+        
     }
 
 
