3c4a0c5380ba66edd406a4ddba09e974778f4b23
Noje <9hnoje@gmail.com>
2021-04-22 09:48:32 +0200

    add logic

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonLayout1: UIButton!
     @IBOutlet weak var buttonLayout2: UIButton!
     @IBOutlet weak var buttonLayout3: UIButton!
+    @IBOutlet weak var swipe: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +24,26 @@ class ViewController: UIViewController {
         buttonLayout2.setImage(UIImage(named: "layoutSelected2"), for: .selected)
         buttonLayout3.setImage(UIImage(named: "layoutSelected3"), for: .selected)
         
+        let swipeTop = UISwipeGestureRecognizer(target: self, action: #selector(swipe.returnToSwipeGesture))
+        swipeTop.direction = UISwipeGestureRecognizer.Direction.up
+        swipe.view.addGestureRecognizer(swipeTop)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: swipe, action: #selector(swipe.returnToSwipeGesture))
+        swipeTop.direction = UISwipeGestureRecognizer.Direction.left
+        swipe.view.addGestureRecognizer(swipeLeft)
     }
-
-
+    
+    func returnToSwipeGesture(gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Up:
+                print("swipe top")
+            case  UISwipeGestureRecognizer.Left:
+                print("swipe left")
+            default:
+                break
+            }
+        }
+    }
 }
 
